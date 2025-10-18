0174e2b990faa71ecf6edc341df715953e0a89a9
Guillermo <hansolowireless@users.noreply.github.com>
2025-10-18 05:42:02 +0200

    El foco se va a la parrilla de canales en la pantalla Channels al navegar a la sección

--- app/src/main/java/com/mamm/mammapps/ui/component/channels/ChannelGridTV.kt
+++ app/src/main/java/com/mamm/mammapps/ui/component/channels/ChannelGridTV.kt
@@ -7,7 +7,12 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.focus.requestFocus
 import com.mamm.mammapps.ui.component.common.ContentEntity
 import com.mamm.mammapps.ui.component.common.ProvideLazyListPivotOffset
 import com.mamm.mammapps.ui.model.ContentEntityUI
@@ -20,6 +25,15 @@ fun ChannelGridTV(
     onChannelClick: (ContentEntityUI) -> Unit,
     onChannelFocus: (ContentEntityUI) -> Unit = {}
 ) {
+
+    val focusRequester = remember { FocusRequester() }
+
+    LaunchedEffect(channels) {
+        if (channels.isNotEmpty()) {
+            focusRequester.requestFocus()
+        }
+    }
+
     ProvideLazyListPivotOffset (parentFraction = 0.15f) {
         LazyVerticalGrid(
             columns = GridCells.Fixed(4),
@@ -31,8 +45,10 @@ fun ChannelGridTV(
             horizontalArrangement = Arrangement.spacedBy(Dimensions.paddingMedium),
             verticalArrangement = Arrangement.spacedBy(Dimensions.paddingSmall)
         ) {
-            items(channels) { channel ->
+            items(channels.size) { index ->
+                val channel = channels[index]
                 ContentEntity(
+                    modifier = if (index == 0) Modifier.focusRequester(focusRequester) else Modifier,
                     contentEntityUI = channel,
                     onClick = { onChannelClick(channel) },
                     onFocus = { onChannelFocus(channel) }
