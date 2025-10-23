94f8909b87f615e0428b9f8f64fadf2a0a311acb
Guillermo <hansolowireless@users.noreply.github.com>
2025-10-23 08:45:16 +0200

    KAN-58 logo infantil

---
+++ app/src/main/java/com/mamm/mammapps/ui/component/icon/Child.kt
@@ -0,0 +1,96 @@
+package com.mamm.mammapps.ui.component.icon
+
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.vector.path
+import androidx.compose.ui.unit.dp
+
+val Child_care: ImageVector
+    get() {
+        if (_Child_care != null) return _Child_care!!
+
+        _Child_care = ImageVector.Builder(
+            name = "Child_care",
+            defaultWidth = 24.dp,
+            defaultHeight = 24.dp,
+            viewportWidth = 960f,
+            viewportHeight = 960f
+        ).apply {
+            path(
+                fill = SolidColor(Color(0xFF000000))
+            ) {
+                moveTo(580f, 470f)
+                quadToRelative(-21f, 0f, -35.5f, -14.5f)
+                reflectiveQuadTo(530f, 420f)
+                reflectiveQuadToRelative(14.5f, -35.5f)
+                reflectiveQuadTo(580f, 370f)
+                reflectiveQuadToRelative(35.5f, 14.5f)
+                reflectiveQuadTo(630f, 420f)
+                reflectiveQuadToRelative(-14.5f, 35.5f)
+                reflectiveQuadTo(580f, 470f)
+                moveToRelative(-200f, 0f)
+                quadToRelative(-21f, 0f, -35.5f, -14.5f)
+                reflectiveQuadTo(330f, 420f)
+                reflectiveQuadToRelative(14.5f, -35.5f)
+                reflectiveQuadTo(380f, 370f)
+                reflectiveQuadToRelative(35.5f, 14.5f)
+                reflectiveQuadTo(430f, 420f)
+                reflectiveQuadToRelative(-14.5f, 35.5f)
+                reflectiveQuadTo(380f, 470f)
+                moveToRelative(100f, 210f)
+                quadToRelative(-60f, 0f, -108.5f, -33f)
+                reflectiveQuadTo(300f, 560f)
+                horizontalLineToRelative(360f)
+                quadToRelative(-23f, 54f, -71.5f, 87f)
+                reflectiveQuadTo(480f, 680f)
+                moveToRelative(0f, 160f)
+                quadToRelative(-75f, 0f, -140.5f, -28.5f)
+                reflectiveQuadToRelative(-114f, -77f)
+                reflectiveQuadToRelative(-77f, -114f)
+                reflectiveQuadTo(120f, 480f)
+                reflectiveQuadToRelative(28.5f, -140.5f)
+                reflectiveQuadToRelative(77f, -114f)
+                reflectiveQuadToRelative(114f, -77f)
+                reflectiveQuadTo(480f, 120f)
+                reflectiveQuadToRelative(140.5f, 28.5f)
+                reflectiveQuadToRelative(114f, 77f)
+                reflectiveQuadToRelative(77f, 114f)
+                reflectiveQuadTo(840f, 480f)
+                reflectiveQuadToRelative(-28.5f, 140.5f)
+                reflectiveQuadToRelative(-77f, 114f)
+                reflectiveQuadToRelative(-114f, 77f)
+                reflectiveQuadTo(480f, 840f)
+                moveToRelative(0f, -80f)
+                quadToRelative(116f, 0f, 198f, -82f)
+                reflectiveQuadToRelative(82f, -198f)
+                reflectiveQuadToRelative(-82f, -198f)
+                reflectiveQuadToRelative(-198f, -82f)
+                horizontalLineToRelative(-12f)
+                quadToRelative(-6f, 0f, -12f, 2f)
+                quadToRelative(-6f, 6f, -8f, 13f)
+                reflectiveQuadToRelative(-2f, 15f)
+                quadToRelative(0f, 21f, 14.5f, 35.5f)
+                reflectiveQuadTo(496f, 280f)
+                quadToRelative(9f, 0f, 16.5f, -3f)
+                reflectiveQuadToRelative(15.5f, -3f)
+                quadToRelative(12f, 0f, 20f, 9f)
+                reflectiveQuadToRelative(8f, 21f)
+                quadToRelative(0f, 23f, -21.5f, 29.5f)
+                reflectiveQuadTo(496f, 340f)
+                quadToRelative(-45f, 0f, -77.5f, -32.5f)
+                reflectiveQuadTo(386f, 230f)
+                verticalLineToRelative(-6f)
+                quadToRelative(0f, -3f, 1f, -8f)
+                quadToRelative(-83f, 30f, -135f, 101f)
+                reflectiveQuadToRelative(-52f, 163f)
+                quadToRelative(0f, 116f, 82f, 198f)
+                reflectiveQuadToRelative(198f, 82f)
+                moveToRelative(0f, -280f)
+            }
+        }.build()
+
+        return _Child_care!!
+    }
+
+private var _Child_care: ImageVector? = null
