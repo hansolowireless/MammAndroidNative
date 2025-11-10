42f0d082b6ff8848db409d2a1c49a6a2bfb15152
Guillermo <hansolowireless@users.noreply.github.com>
2025-11-10 12:33:56 +0100

    Mapper para sacar el mensaje de los errores que llgan por servidor

--- app/src/main/java/com/mamm/mammapps/ui/mapper/Exception.kt
+++ app/src/main/java/com/mamm/mammapps/ui/mapper/Exception.kt
@@ -1,20 +1,24 @@
 package com.mamm.mammapps.ui.mapper
 
+import com.google.gson.Gson
 import com.mamm.mammapps.R
+import com.mamm.mammapps.data.model.exception.ErrorResponse
 import com.mamm.mammapps.data.model.exception.GetHomeContentException
-
-fun Throwable.toResId() : Int {
-    return when (this) {
-        is GetHomeContentException -> this.toResId()
-        else -> R.string.generic_error
-    }
-}
+import retrofit2.HttpException
 
 fun GetHomeContentException.toResId() : Int {
     return when (this) {
         is GetHomeContentException.ForbiddenException -> R.string.error_subscription_in_process
         is GetHomeContentException.GenericException -> R.string.error_content_generic
     }
+}
+
+fun HttpException?.toResponseBodyMessage() : String {
+    val result = runCatching {
+        val errorBody = this?.response()?.errorBody()?.string()
+        Gson().fromJson(errorBody, ErrorResponse::class.java).message
+    }
+    return result.getOrNull() ?: this?.message().orEmpty()
 }
 
 
