8daafa09661f55fccbfa30e8cd8dc441fe296e2c
Guillermo <hansolowireless@users.noreply.github.com>
2025-12-09 20:49:09 +0100

    Quitar menú lateral en categoría expandida

--- app/src/main/java/com/mamm/mammapps/navigation/Util.kt
+++ app/src/main/java/com/mamm/mammapps/navigation/Util.kt
@@ -29,22 +29,82 @@ object MenuItems {
     fun GetIconForRoute(route: AppRoute) {
         val iconModifier = Modifier.size(24.dp)
         when (route) {
-            AppRoute.HOME -> Icon(Icons.Default.Home, contentDescription = null, modifier = iconModifier)
+            AppRoute.HOME -> Icon(
+                Icons.Default.Home,
+                contentDescription = null,
+                modifier = iconModifier
+            )
+
             AppRoute.EPG -> Icon(BulletedList, contentDescription = null, modifier = iconModifier)
-            AppRoute.CHANNELS -> Icon(painterResource(id = R.drawable.menu_remoteicon), contentDescription = null, modifier = iconModifier)
-            AppRoute.MOVIES -> Icon(painterResource(id = R.drawable.menu_cinemaicon2), contentDescription = null, modifier = iconModifier)
-            AppRoute.DOCUMENTARIES -> Icon(painterResource(id = R.drawable.menu_documentariesicon), contentDescription = null, modifier = iconModifier)
-            AppRoute.SERIES -> Icon(painterResource(id = R.drawable.menu_serieslogoicon), contentDescription = null, modifier = iconModifier)
-            AppRoute.WARNER -> Icon(painterResource(id = R.drawable.menu_wblogoicon), contentDescription = null, modifier = iconModifier)
-            AppRoute.ACONTRA -> Icon(painterResource(id = R.drawable.menu_acontralogoicon), contentDescription = null, modifier = iconModifier)
-            AppRoute.AMC -> Icon(painterResource(id = R.drawable.menu_amclogoicon), contentDescription = null, modifier = iconModifier)
-            AppRoute.SPORTS -> Icon(painterResource(id = R.drawable.menu_iconfootball), contentDescription = null, modifier = iconModifier)
+            AppRoute.CHANNELS -> Icon(
+                painterResource(id = R.drawable.menu_remoteicon),
+                contentDescription = null,
+                modifier = iconModifier
+            )
+
+            AppRoute.MOVIES -> Icon(
+                painterResource(id = R.drawable.menu_cinemaicon2),
+                contentDescription = null,
+                modifier = iconModifier
+            )
+
+            AppRoute.DOCUMENTARIES -> Icon(
+                painterResource(id = R.drawable.menu_documentariesicon),
+                contentDescription = null,
+                modifier = iconModifier
+            )
+
+            AppRoute.SERIES -> Icon(
+                painterResource(id = R.drawable.menu_serieslogoicon),
+                contentDescription = null,
+                modifier = iconModifier
+            )
+
+            AppRoute.WARNER -> Icon(
+                painterResource(id = R.drawable.menu_wblogoicon),
+                contentDescription = null,
+                modifier = iconModifier
+            )
+
+            AppRoute.ACONTRA -> Icon(
+                painterResource(id = R.drawable.menu_acontralogoicon),
+                contentDescription = null,
+                modifier = iconModifier
+            )
+
+            AppRoute.AMC -> Icon(
+                painterResource(id = R.drawable.menu_amclogoicon),
+                contentDescription = null,
+                modifier = iconModifier
+            )
+
+            AppRoute.SPORTS -> Icon(
+                painterResource(id = R.drawable.menu_iconfootball),
+                contentDescription = null,
+                modifier = iconModifier
+            )
+
             AppRoute.ADULTS -> Icon(Fire, contentDescription = null, modifier = iconModifier)
             AppRoute.KIDS -> Icon(Puzzle, contentDescription = null, modifier = iconModifier)
-            AppRoute.SEARCH -> Icon(Icons.Default.Search, contentDescription = null, modifier = iconModifier)
+            AppRoute.SEARCH -> Icon(
+                Icons.Default.Search,
+                contentDescription = null,
+                modifier = iconModifier
+            )
+
             AppRoute.ABOUT -> Icon(WifiSignal, contentDescription = null, modifier = iconModifier)
-            AppRoute.LOGOUT -> Icon(Icons.Default.Person, contentDescription = null, modifier = iconModifier)
-            AppRoute.EXIT -> Icon(Icons.AutoMirrored.Filled.ExitToApp, contentDescription = null, modifier = iconModifier)
+            AppRoute.LOGOUT -> Icon(
+                Icons.Default.Person,
+                contentDescription = null,
+                modifier = iconModifier
+            )
+
+            AppRoute.EXIT -> Icon(
+                Icons.AutoMirrored.Filled.ExitToApp,
+                contentDescription = null,
+                modifier = iconModifier
+            )
+
             else -> {} // Para otras rutas como Login, etc.
         }
     }
@@ -72,16 +132,19 @@ object MenuItems {
     val listNoBrandedContent = list.filter {
         it != AppRoute.WARNER
                 && it != AppRoute.AMC
-                && it != AppRoute.ACONTRA }
+                && it != AppRoute.ACONTRA
+    }
 
     fun showSideMenu(route: AppRoute): Boolean {
         return when (route) {
             LOGOUT,
             AppRoute.PLAYER,
             AppRoute.DETAIL,
+            AppRoute.EXPANDCATEGORY,
             AppRoute.LOGIN,
             AppRoute.EXIT,
             AppRoute.ERROR_SUBSCRIPTION_ONGOING -> false
+
             else -> true
         }
     }
