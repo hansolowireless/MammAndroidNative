d2d47e18fcb0fa357fe1fc2fc12d28aa0885ad72
Guillermo <hansolowireless@users.noreply.github.com>
2025-12-17 13:27:41 +0100

    version 4.8.011 - 2025-12-17

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -92,8 +92,8 @@ android {
         applicationId = "com.mamm.mammapps"
         minSdk = 21
         targetSdk = 35
-        versionCode = 513
-        versionName = "4.8.010"
+        versionCode = 514
+        versionName = "4.8.011"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
